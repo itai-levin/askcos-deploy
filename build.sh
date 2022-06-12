fbbff9ab476e3d574b605f8fc788084518b8e5ba
Itai Levin <itail@mit.edu>
2022-06-12 09:35:08 +0000

    Changed path to data

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ sudo make TAG=2020.07 CORE_VERSION=2020.07
 cd ../askcos-deploy
 sudo bash deploy.sh deploy -v 2020.07 --local
 
-sudo bash deploy.sh seed-db -r data/templates/bkms-and-reaxys-templates.json.gz -x data/reactions/bkms-reactions.json.gz
+sudo bash deploy.sh seed-db -r bkms-data/templates/bkms-and-reaxys-templates.json.gz -x bkms-data/reactions/bkms-reactions.json.gz
 
 sudo ./set_up_model_bkms.sh
--- set_up_model_bkms.sh
+++ set_up_model_bkms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run -d --name serving_base tensorflow/serving
-docker cp data/models/bkms serving_base:/models/template_relevance
+docker cp bkms-data/models/bkms serving_base:/models/template_relevance
 docker commit --change "ENV MODEL_NAME template_relevance" serving_base template-relevance-bkms
 docker-compose up -d template-relevance-bkms
